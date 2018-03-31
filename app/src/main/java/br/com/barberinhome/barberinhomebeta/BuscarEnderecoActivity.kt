e00326d560e760c99e4ac851e670b18b4464dccb
Otavio <persistere.5@gmail.com>
2018-03-30 21:11:57 -0300

    Inserindo Tela de localizacao

---
+++ app/src/main/java/br/com/barberinhome/barberinhomebeta/BuscarEnderecoActivity.kt
@@ -0,0 +1,15 @@
+package br.com.barberinhome.barberinhomebeta
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BuscarEnderecoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buscar_endereco)
+    }
+
+
+}
--- app/src/main/java/br/com/barberinhome/barberinhomebeta/CadastrarActivity.kt
+++ app/src/main/java/br/com/barberinhome/barberinhomebeta/CadastrarActivity.kt
@@ -1,12 +1,21 @@
 package br.com.barberinhome.barberinhomebeta
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastrar.*
 
 class CadastrarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastrar)
+
+        btSalvarCadastro.setOnClickListener {
+            val intent = Intent(this, BuscarEnderecoActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
